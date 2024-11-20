a382a648372b0c9bc411f51267818bc2af1c411b
hhhssskkkhqx <huangqingxian@hashkey.com>
2024-11-20 17:23:11 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+export const kits = [
+    {
+      "index": 1,
+      "svg": "",
+      "png": ""
+    },
+    {
+      "index": 2,
+      "svg": "",
+      "png": ""
+    },
+    {
+      "index": 3,
+      "svg": "",
+      "png": ""
+    },
+    {
+      "index": 4,
+      "svg": "",
+      "png": ""
+    }
+  ]
